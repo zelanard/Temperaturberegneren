fd9921c55df1c9b8b280d9781946150bfa5aefa3
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-08-10 14:11:52 +0200

    Moved Calculations to methods

--- Temperaturberegneren/Program.cs
+++ Temperaturberegneren/Program.cs
@@ -14,12 +14,12 @@
             {
                 try
                 {
-                    //asign values to the tempratures.
+                    //Asign values to the tempratures.
                     Celcius = float.Parse(Console.ReadLine());
-                    Fahrenheit = (Celcius * 1.8F) + 32F;
-                    Reamur = (Celcius * 0.8F);
+                    Fahrenheit = ToFahrenheit(Celcius);
+                    Reamur = ToReamur(Celcius);
 
-                    //write the asigned values to the console.
+                    //Write the asigned values to the console.
                     Console.WriteLine(Celcius + " °C = " + Fahrenheit + " °F");
                     Console.WriteLine(Celcius + " °C = " + Reamur + " °R");
                 }
@@ -29,6 +29,26 @@
                     Console.WriteLine("Error! You did not write an appropriate temprature!");
                 }
             }
+        }
+
+        /// <summary>
+        /// Converts Celcius To Reamur.
+        /// </summary>
+        /// <param name="Celcius"></param>
+        /// <returns>The value in Reamur as a float</returns>
+        private static float ToReamur(float Celcius)
+        {
+            return (Celcius * 0.8F);
+        }
+
+        /// <summary>
+        /// Converts Clecius to Fahrenheit.
+        /// </summary>
+        /// <param name="Celcius"></param>
+        /// <returns>The value in Fahrenheit as a float</returns>
+        private static float ToFahrenheit(float Celcius)
+        {
+            return (Celcius * 1.8F) + 32F;
         }
     }
 }
