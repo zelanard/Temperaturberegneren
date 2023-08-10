9ac9b8e5e0b327b692e90fcbca29fa5359c0a618
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-08-10 13:32:10 +0200

    Add project files.

---
+++ Temperaturberegneren/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Temperaturberegneren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define Temperatur Scale
+            float Celcius;
+            float Fahrenheit;
+            float Reamur;
+
+            //Make sure the program can run more than once by placing it in a while loop.
+            while (true)
+            {
+                try
+                {
+                    //asign values to the tempratures.
+                    Celcius = float.Parse(Console.ReadLine());
+                    Fahrenheit = (Celcius * 1.8F) + 32F;
+                    Reamur = (Celcius * 0.8F);
+
+                    //write the asigned values to the console.
+                    Console.WriteLine(Celcius + " °C = " + Fahrenheit + " °F");
+                    Console.WriteLine(Celcius + " °C = " + Reamur + " °R");
+                }
+                catch
+                {
+                    //If a wrong temprature was written, give an error message.
+                    Console.WriteLine("Error! You did not write an appropriate temprature!");
+                }
+            }
+        }
+    }
+}
